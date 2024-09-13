63909301ff21c6c6a6b4dcb8aa17afa0aeed3bac
rizkyakbar1511 <rizkyakbar1511@gmail.com>
2024-09-13 14:46:29 +0700

    chore: add client env

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,8 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import "./index.css";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri:
+    import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql",
   cache: new InMemoryCache(), //Apollo client uses to cache query results after fetching them.
   credentials: "include", // this tells Apollo client to send cookies along with every request to the server.
 });
