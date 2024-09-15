cce812eb7201337708104e8d035e3cb1bde14c95
rizkyakbar1511 <rizkyakbar1511@gmail.com>
2024-09-15 08:31:59 +0700

    fix: __filename error

--- server/modules/typedefs.ts
+++ server/modules/typedefs.ts
@@ -8,7 +8,7 @@ import path from "path";
 /**
  * do this manual dirname with es type "module" set in package.json
  */
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 // Load and merge all .graphql files
